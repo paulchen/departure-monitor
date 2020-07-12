ca40999a968455a69b721e63b58af16a0ce667d1
Paul Staroch <paulchen@rueckgr.at>
2020-07-12 21:41:19 +0200

    implement 'compact' view mode

--- src/app/monitor/monitor.component.ts
+++ src/app/monitor/monitor.component.ts
@@ -16,10 +16,41 @@ export class MonitorComponent implements OnInit {
 
   stationDetails: StationDetails;
   rblData: { [rbl: string]: Departure[] } = {};
+  rblDataCompact: { [rbl: string]: Departure[] } = {};
   loading = false;
+  compact = true;
   timeout;
 
   constructor(private rblService: RblService) { }
+
+  private static getCompactRblData(rblData: { [rbl: string]: Departure[] }): { [rbl: string]: Departure[] } {
+    const result: { [rbl: string]: Departure[] } = {};
+    Object.entries(rblData).forEach(([key, value]) => {
+      result[key] = MonitorComponent.getCompactRbl(value);
+    });
+    return result;
+  }
+
+  private static getCompactRbl(departures: Departure[]): Departure[] {
+    const result: Departure[] = [];
+    const multipleLines = MonitorComponent.countLines(departures) > 1;
+    const entriesByLine: { [line: string]: number} = {};
+    departures.forEach(departure => {
+      const line = departure.line;
+      if(!entriesByLine.hasOwnProperty(line)) {
+        entriesByLine[line] = 0;
+      }
+      entriesByLine[line] = entriesByLine[line] + 1;
+      if((multipleLines && entriesByLine[line] <= 1) || (!multipleLines && entriesByLine[line] <= 2)) {
+        result.push(departure);
+      }
+    });
+    return result;
+  }
+
+  private static countLines(departures: Departure[]): number {
+    return new Set(departures.map(d => d.line)).size;
+  }
 
   ngOnInit(): void {
     this.rblService.getStationDetails(this.station).subscribe(stationDetails => {
@@ -45,6 +76,7 @@ export class MonitorComponent implements OnInit {
     this.rblService.getDepartureData(rbls).subscribe(data => {
       this.loading = false;
       this.rblData = data;
+      this.rblDataCompact = MonitorComponent.getCompactRblData(data);
       this.timeout = setTimeout(() => { this.updateMonitor(); }, 30000);
     });
   }
