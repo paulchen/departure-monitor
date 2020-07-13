a1187a2c02f678d5a6bae92996064b1527f79ba8
Paul Staroch <paulchen@rueckgr.at>
2020-07-13 21:15:04 +0200

    introduce router

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,15 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import {SearchComponent} from './search/search.component';
+import {MonitorComponent} from './monitor/monitor.component';
+import {PageNotFoundComponent} from './page-not-found/page-not-found.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', component: SearchComponent, data: {routeName: 'Search'}},
+  {path: 'station/:id/:name', component: MonitorComponent, data: {routeName: 'Station'}},
+  {path: '**', component: PageNotFoundComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
